fa9b4ef91c035c207aaa55a451b499b8ad1d59ca
Diego Ramirez <diegoadrian_rmz@hotmail.com>
2024-05-21 11:11:28 -0600

    Haciendo buscador de Wiki

--- code/js/main.js
+++ code/js/main.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo desde 11ty 🦡🎈");
+document.addEventListener("keyup", e=>{
+
+  if (e.target.matches("#buscador")){
+
+      if (e.key ==="Escape")e.target.value = ""
+
+      document.querySelectorAll(".articulo").forEach(article =>{
+
+          article.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?article.classList.remove("filtro")
+            :article.classList.add("filtro")
+      })
+
+  }
+
+
+})
